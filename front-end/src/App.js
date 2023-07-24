ace7792f7536d2abf41cde72f398fbbbe8e50a9e
Tom Charon <charon.s.tom@gmail.com>
2023-07-24 12:42:44 +0200

    front end login system done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,14 +4,49 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
+  Navigate,
+  Outlet,
 } from "react-router-dom";
+
+import "./sass/main.scss";
 import Login from "./pages/log-in/Login";
 import Signup from "./pages/sign-up/Signup";
-import "./sass/main.scss";
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+const connected = false;
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const PrivateRoute = ({ children }) => {
+  if (!connected) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
 
 const rooter = createBrowserRouter(
   createRoutesFromElements(
     <>
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <Layout />
+          </PrivateRoute>
+        }
+      >
+        <Route path="/" element={<Home />} />
+      </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
     </>
--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-hi;
-gyg;
+const express = require("express");
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log(`le serveur est lancée sur le port : ${5000}`);
+});
